3f06008285f895367bc604981ad73b2db797108f
GIDIGI VENKATA KISHORE <121595860+kishore631@users.noreply.github.com>
2026-01-14 11:21:49 +0530

    Fix import path module syntax in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import path from 'node:path';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
